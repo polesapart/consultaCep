347c0018a9309bfb13b7c0c53261459f16d631f9
Alexandre Pereira Nunes <alexandre.nunes@gmail.com>
2023-09-13 16:28:31 -0300

    further updates & fixes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,16 +18,16 @@ repositories {
 
 dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
     testImplementation(kotlin("test-junit"))
     testImplementation(kotlin("test"))
     testImplementation("com.squareup.retrofit2:retrofit:2.9.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-    testImplementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
-    testImplementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    testImplementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    testImplementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
 
 tasks.test {
@@ -42,6 +42,12 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs += listOf("-Xopt-in=kotlin.RequiresOptIn", "-Xjsr305=strict")
     }
 }
+tasks.withType<Test> {
+    /**
+     * fix for retrofit https://github.com/square/retrofit/issues/3341
+     */
+    jvmArgs = listOf("--add-opens", "java.base/java.lang.invoke=ALL-UNNAMED")
+}
 
 val compileTestKotlin: KotlinCompile by tasks
 
@@ -53,6 +59,6 @@ java {
     withSourcesJar()
     withJavadocJar()
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion.set(JavaLanguageVersion.of(11))
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,6 @@
 rootProject.name = "consultaCep"
 
+// settings.gradle.kts
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
